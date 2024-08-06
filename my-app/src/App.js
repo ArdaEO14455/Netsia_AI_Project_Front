57af53b03a64810a7158ba374cc09dd1470443a9
ArdaEO14455 <14455@coderacademy.edu.au>
2024-08-06 13:56:51 +1000

    basic skeleton created

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,10 @@
+import './App.css';
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
+
   const [messages, setMessages] = useState([]);
   const [input, setInput] = useState('');
 
@@ -15,36 +17,42 @@ function App() {
       // Add mock response
       setMessages(prevMessages => [
         ...prevMessages,
-        { user: 'User', text: input },
-        { user: 'Response', text: 'test response' }
+        // { user: 'User', text: input },
+        { user: 'ChatGPT', text: 'test response' }
       ]);
     }
   };
 
+  
   return (
-    
-    <div className="container">
+    <div className="App">
+      <header className="App-header">
+      <div className="container">
 
-      {/* Nav Bar */}
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+{/* Chat History Column */}
+
+  <div class="column align-items-start">
+    <div class="col">
+      Chat History
+    </div>
+  </div>
+
+{/* Nav + User Prompt */}
+
+<div class="nav-chat-container">
+
+{/* Nav Bar */}
+
+      <nav class="navbar">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="nav-item" href="#">Netsia</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          <a class="nav-link active" aria-current="page" href="#">Login</a>
         </li>
       </ul>
     </div>
@@ -55,21 +63,25 @@ function App() {
       <div className="chat-container">
         <div className="messages">
           {messages.map((msg, index) => (
-            <div key={index} className={`message ${msg.user.toLowerCase()}`}>
-              <strong>{msg.user}: </strong>{msg.text}
+            <div class ="dialogue" key={index} className={`message ${msg.user.toLowerCase()}`}>
+              <strong class="dialogue">{msg.user}: </strong >{msg.text}
             </div>
           ))}
         </div>
-        <div className="input-container">
+      </div>
+      <div className="input-container ">
           <textarea
-            className="form-control"
+            class="form-control"
             rows="3"
             value={input}
             onChange={(e) => setInput(e.target.value)}
           />
           <button className="btn btn-primary" onClick={handleSend}>Send</button>
         </div>
-      </div>
+    </div>
+    </div>
+  
+      </header>
     </div>
   );
 }
