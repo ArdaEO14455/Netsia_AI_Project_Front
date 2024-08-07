52e80712dfe0fe51bdd9f0a4b5bfd9dede3a67de
ArdaEO14455 <14455@coderacademy.edu.au>
2024-08-07 10:38:51 +1000

    updated input field layout

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -60,14 +60,19 @@ function App() {
       
       
         </div>
-        <div className="input-container ">
+        <div className="input-container mx-auto ">
           <textarea
             class="form-control bg-transparent text-light"
             rows="3"
             value={input}
             onChange={(e) => setInput(e.target.value)}
           />
-          <button className="btn btn-secondary" onClick={handleSend}>Send</button>
+          <button className="btn btn-secondary" onClick={handleSend}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-arrow-up-circle" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z"/>
+          </svg>
+          </button>
+
         </div>
     </div>
     </div>
