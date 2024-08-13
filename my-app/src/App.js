56c97f566c4f80ee64f16fad8f057b72e8f41541
ArdaEO14455 <14455@coderacademy.edu.au>
2024-08-13 14:04:43 +1000

    testing conversation select & display

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,16 +7,18 @@ import './App.css';
 function App() {
   const [messages, setMessages] = useState([]);
   const [input, setInput] = useState('');
+  const [conversations, setConversations] = useState({});
+  const [selectedConversation, setSelectedConversation] = useState([]);
 
-  const apiKey = process.env.REACT_APP_API_KEY
+  const apiKey = process.env.REACT_APP_API_KEY;
 
-  // Fetch conversation history from the back-end
+  // Fetch conversation history and all conversations from the back-end
   useEffect(() => {
-    fetch(`${apiKey}/conversation-history`) // Update the URL as needed
+    fetch(`${apiKey}/allConversations`)
       .then(response => response.json())
-      .then(data => setMessages(data))
-      .catch(error => console.error('Error fetching conversation:', error));
-  }, []);
+      .then(data => setConversations(data))
+      .catch(error => console.error('Error fetching conversations:', error));
+  }, [apiKey]);
 
   const handleSend = () => {
     if (input.trim() !== '') {
@@ -32,71 +34,53 @@ function App() {
     }
   };
 
+  const handleConversationSelect = (conversationKey) => {
+    setSelectedConversation(conversations[conversationKey]?.messages || []);
+  };
+
   return (
-    <div class="App">
-  <header class="App-header">
-    <div class="container-fluid">
-  
+    <div className="App">
+      <header className="App-header">
+        <div className="container-fluid">
 
+          {/* Side Bar */}
+          <div className="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="side-bar-mobile" aria-labelledby="offcanvasWithBothOptionsLabel">
+            <div className="offcanvas-header">
+              <h5 className="offcanvas-title text-light" id="offcanvasWithBothOptionsLabel">Chat History</h5>
+              <button type="button" className="btn-close bg-secondary" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div className="offcanvas-body text-light">
+              <ul>
+                {Object.keys(conversations).map(key => (
+                  <li key={key} className="nav-link" onClick={() => handleConversationSelect(key)}>
+                    {conversations[key].name}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
 
-{/* Side Bar Mobile */}
+          {/* Nav + User Prompt */}
+          <div className="nav-chat-container ">
 
-<div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="side-bar-mobile" aria-labelledby="offcanvasWithBothOptionsLabel">
-  <div class="offcanvas-header">
-    <h5 class="offcanvas-title text-light" id="offcanvasWithBothOptionsLabel">Chat History</h5>
-    <button type="button" class="btn-close bg-secondary" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-  </div>
-  <div class="offcanvas-body text-light">
-    <p>Try scrolling the rest of the page to see this option in action.</p>
-  </div>
-</div>
-
-
-
-
-
-  
-
-{/* Nav + User Prompt */}
-
-<div class="nav-chat-container ">
-
-{/* Nav Bar */}
-
-<nav class="navbar px-2">
-
-
-<div id="button-container d-none d-lg-block d-md-block">
-  
-{/* Mobile Side-Bar Button */}
-  <button 
-  class="btn active border-0" 
-  // id="button-container" 
-  type="button" 
-  data-bs-toggle="offcanvas" 
-  data-bs-target="#side-bar-mobile" 
-  aria-controls="side-bar-mobile">
-    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-      <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
-    </svg>
-  </button>
-
-
-
-</div>
-
-
-
-
-
-    <a class="nav-link fw-bold" href="#">NETSIA</a>
-    <a class="nav-link" href="#">Login</a>
-</nav>
+            {/* Nav Bar */}
+            <nav className="navbar px-2">
+              <div id="button-container d-none d-lg-block d-md-block">
+                {/* Mobile Side-Bar Button */}
+                <button className="btn active border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#side-bar-mobile" aria-controls="side-bar-mobile">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+                    <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+                  </svg>
+                </button>
+              </div>
+              <a className="nav-link fw-bold" href="#">NETSIA</a>
+              <a className="nav-link" href="#">Login</a>
+            </nav>
 
             {/* Chat Container */}
             <div className="chat-container mx-1 border-0">
               <div className="messages mx-1">
-                {messages.map((msg, index) => (
+                {selectedConversation.map((msg, index) => (
                   <div key={index} className={`message ${msg.user.toLowerCase()} px-5 fs-5`}>
                     {msg.text}
                   </div>
